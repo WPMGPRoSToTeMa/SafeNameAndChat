66c4569e19fbb19e0f16b0e08769bfa294125d2b
Artem <Artem@026ce923-eb23-4acc-b252-e579ea3b0314>
2016-01-25 02:09:11 +0000

    Initial commit

---
+++ Main.h
@@ -0,0 +1,3 @@
+#pragma once
+
+const char PLUGIN_VERSION[] = "1.0";
---
+++ hlsdk/engine/archtypes.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+
+typedef int8_t    int8;
+typedef uint8_t   uint8;
+typedef int16_t   int16;
+typedef uint16_t  uint16;
+typedef int32_t   int32;
+typedef uint32_t  uint32;
+typedef int64_t   int64;
+typedef uint64_t  uint64;
+typedef intmax_t  intmax;
+typedef uintmax_t uintmax;
+typedef intptr_t  intptr;
+typedef uintptr_t uintptr;
+typedef ptrdiff_t ptrdiff;
